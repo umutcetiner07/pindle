1df5f416fa816e5d7d65800453f54d8f6d5d6319
umutcetiner07 <umutcetiner@gmail.com>
2025-06-23 15:29:17 +0300

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,26 @@
+
+<template>
+  <div id="app">
+    <h1>Pindle</h1>
+    <p>Welcome to the Pi-powered e-commerce platform.</p>
+    <button @click="createStore">Create Store</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  methods: {
+    createStore() {
+      alert("Store creation coming soon!");
+    }
+  }
+};
+</script>
+
+<style>
+#app {
+  text-align: center;
+  margin-top: 50px;
+}
+</style>
